3d5400ab4da3919272a6e08dfeaaed9c8ca51348
nurealammiaji <nurealam.miaji@gmail.com>
2023-10-21 20:50:11 +0600

    mongodb configured and get function added

--- index.js
+++ index.js
@@ -1,18 +1,64 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Auto Repair Server");
+  res.send("Auto Repair Server");
 })
 
 app.listen(port, () => {
-    console.log(`Auto Repair Server running on port: ${port}`);
+  console.log(`Auto Repair Server running on port: ${port}`);
 })
 
+const uri = `mongodb+srv://${username}:${password}@cluster0.31s3qjy.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+
+    const autoRepairCollection = client.db("autoRepair").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const cursor = autoRepairCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get("/services/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await autoRepairCollection.findOne(query);
+      res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
