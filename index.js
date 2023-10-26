a423d98a0f6134e995368156f0f722fb0435e9d1
nurealammiaji <nurealam.miaji@gmail.com>
2023-10-26 22:09:52 +0600

    jwt installed

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 const username = process.env.DB_USER;
 const password = process.env.DB_PASS;
+const accessToken= process.env.ACCESS_TOKEN_SECRET;
+
+console.log(accessToken);
 
 app.use(cors());
 app.use(express.json());
